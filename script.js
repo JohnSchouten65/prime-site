030c91733927302513082be7bb872ae485aea1ca
Johannes Petrus Schouten <john.schouten65@gmail.com>
2025-07-30 11:16:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
+function checkPrime() {
+  const num = parseInt(document.getElementById("numberInput").value);
+  const result = isPrime(num) ? num + " is PRIME" : num + " is NOT prime";
+  document.getElementById("result").textContent = result;
+}
+
+function findNextPrime() {
+  let num = parseInt(document.getElementById("numberInput").value);
+  do {
+    num++;
+  } while (!isPrime(num));
+  document.getElementById("result").textContent = "Next prime: " + num;
+}
